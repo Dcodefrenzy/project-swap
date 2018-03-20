6fc10afc997934741f3199d4fabe77a74d65487d
Dcodefrenzy <ayodejisamuelfakunle@gmail.com>
2018-03-20 20:56:03 +0100

    Starting A New Project

---
+++ signup.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Bank Registration Form</title>
+</head>
+
+<body>
+
+	<h1 align="center">Sign Up Form</h1>
+    <h3>Welcome to Our resgistration process</h3>
+    <p>After you have submitted your account registration form, you will receive an email with your informations thank you.</p>
+    <p>Please fill the form bellow.</p>
+    <form action="" method="">
+    	<p>First Name: <input type="text" /></p>
+        <p>Last Name:  <input type="text" /></p>
+        <p>Middle Name:<input type="text" /></p>
+       <p>Date Of Birth:<input type="date" /></p>
+        <p>Adress: <br /><textarea rows="10" cols="20"> </textarea></p>
+         <p>Phone Number:<input type="text" /></p>
+         <p>Email:<input type="email" /></p>
+         <p>Sex: Male <input type="radio" name="sex"/>
+         		 Female <input type="radio" name="sex"/></p>
+         <p>Nationality:<select>
+         					<option>Select Nationality</option>
+                            <option>Alstrailia</option>
+                            <option>Brazil</option>
+                            <option>Cameroon</option>
+                            <option>Denmark</option>
+                            <option>England</option>
+                            <option>France</option>
+                            <option>Germany</option>
+                            <option>Holand</option>
+                            <option>Iran</option>
+                            <option>Jordan</option>
+                            <option>Libya</option>
+                            <option>Mali</option>
+                            <option>Nigeria</option>
+                            <option>Portugal</option>
+                            <option>Russia</option>
+                            <option>Sudan</option>
+                            <option>USA</option>
+                            <option>Zambia</option>
+                      </select></p>
+                       <p>
+                <b>State of Origin:</b>
+            </p>
+            <select name="state-of-origin" id="select">
+                <option>---Select State---</option>
+                <option>ABUJA FCT</option>
+                <option>ABIA</option><option>ADAMAWA</option>
+                <option>AKWA IBOM</option>
+                <option>ANAMBRA</option>
+                <option>BAUCHI</option>
+                <option>BAYELSA</option>
+                <option>BENUE</option>
+                <option>BORNO</option>
+                <option>CROSS RIVER</option>
+                <option>DELTA</option>
+                <option>EBONYI</option>
+                <option>EDO</option>
+                <option>EKITI</option>
+                <option>ENUGU</option>
+                <option>GOMBE</option>
+                <option>IMO</option>
+                <option>JIGAWA</option>
+                <option>KADUNA</option>
+                <option>KANO</option>
+                <option>KATSINA</option>
+                <option>KEBBI</option>
+                <option>KOGI</option>
+                <option>KWARA</option>
+                <option>LAGOS</option>
+                <option>NASSARAWA</option>
+                <option>NIGER</option>
+                <option>OGUN</option>
+                <option>ONDO</option>
+                <option>OSUN</option>
+                <option>OYO</option>
+                <option>PLATEAU</option>
+                <option>RIVERS</option>
+                <option>SOKOTO</option>
+                <option>TARABA</option>
+                <option>YOBE</option>
+                <option>ZAMFARA</option>
+            </select><p>
+
+                <input type="submit" value="Upload" />
+
+
+    </form>
+</body>
+</html>
